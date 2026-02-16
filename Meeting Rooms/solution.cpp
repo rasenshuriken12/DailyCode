a98aada576e6c2a5e1e2fedcb03c523412d22e44
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-02-16 18:12:24 +0000

    solving Meeting Rooms problem by npci

---
+++ Meeting Rooms/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        sort(arr.begin(),arr.end(),[](vector<int>&a,vector<int>&b){
+            return a[1]<b[1];
+        });
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i][1]>arr[i+1][0]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
